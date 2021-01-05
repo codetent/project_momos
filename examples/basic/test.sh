591fdfc2bf34cdca86c6ad0aa707f0329da38929
codetent <christophsw@gmail.com>
2021-01-05 10:54:13 +0100

    Reorder projects

--- examples/basic/test.sh
+++ examples/basic/test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd tools
-
 python3 -m momos build \
         -i ../src/states.c \
         -o ../test/test_states.cpp \
+        --data-type int \
         --state-var ../src/states.c:current_state \
         --fn-init logic.h:logic_init \
         --fn-update logic.h:logic_run
